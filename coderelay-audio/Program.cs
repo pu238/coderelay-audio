012b5020c61e45c501238846345684099fa6a7a1
David Hart <david.hart555@googlemail.com>
2015-09-21 23:32:20 +0100

    Added wavetable player with on/off/render funcs

--- coderelay-audio/Program.cs
+++ coderelay-audio/Program.cs
@@ -9,34 +9,42 @@ class Program
     {
         Arguments arguments = new Arguments(args);
 
+        const int tableSamples = 200;
         // Generate a single sine wave period
-        const int sineSamples = 200;
-        double[] sineTable = new double[sineSamples];
-        for (int i = 0; i < sineSamples; ++i)
+        double[] sineTable = new double[tableSamples];
+        for (int i = 0; i < tableSamples; ++i)
         {
-            sineTable[i] = Math.Sin(2.0 * Math.PI * (double)i / (double)sineSamples);
+            sineTable[i] = Math.Sin(2.0 * Math.PI * (double)i / (double)tableSamples);
         }
 
-        // Fill a buffer with sine wave
-        double[] sineData = new double[SampleRate * 2];
-        for (int i = 0; i < sineData.Length; ++i)
+        int tones = 10;
+        double[] sineData = new double[tableSamples * tones * 128];
+        WavetablePlayer sineChannel = new WavetablePlayer(sineTable, sineData);
+        for (int i = 0; i < tones; ++i)
         {
-            sineData[i] = sineTable[i % sineSamples];
+            sineChannel.On();
+            sineChannel.Render(tableSamples * 32);
+            sineChannel.Off();
+            sineChannel.Render(tableSamples * 96);
         }
 
         // Generate a single square wave period
-        const int squareSamples = 200;
-        double[] squareTable = new double[squareSamples];
-        for (int i = 0; i < squareSamples; ++i)
+        double[] squareTable = new double[tableSamples];
+        for (int i = 0; i < tableSamples; ++i)
         {
-            squareTable[i] = Math.Sin(2.0 * Math.PI * (double)i / (double)sineSamples) > 0 ? 1 : 0;
+            squareTable[i] = Math.Sin(2.0 * Math.PI * (double)i / (double)tableSamples) > 0 ? 1 : -1;
         }
 
         // Fill a buffer with square wave
-        double[] squareData = new double[SampleRate];
-        for (int i = 0; i < squareData.Length; ++i)
+        double[] squareData = new double[tableSamples * tones * 128];
+        WavetablePlayer squareChanel = new WavetablePlayer(squareTable, squareData);
+        for (int i = 0; i < tones; ++i)
         {
-            squareData[i] = squareTable[i % squareSamples];
+            squareChanel.Render(tableSamples * 64);
+            squareChanel.On();
+            squareChanel.Render(tableSamples * 32);
+            squareChanel.Off();
+            squareChanel.Render(tableSamples * 32);
         }
 
         double[] finalData = Mixer.Mix(sineData, squareData);
---
+++ coderelay-audio/WavetablePlayer.cs
@@ -0,0 +1,40 @@
+﻿class WavetablePlayer
+{
+    double[] wavetable;
+    double[] destbuffer;
+    long offset = 0;
+    bool on;
+
+    public WavetablePlayer(double [] wavetable, double [] destbuffer)
+    {
+        this.wavetable = wavetable;
+        this.destbuffer = destbuffer;
+    }
+
+    public void On()
+    {
+        on = true;
+    }
+
+    public void Off()
+    {
+        on = false;
+    }
+
+    public void Render(int samples)
+    {
+        long newOffset = offset + samples;
+
+        if (newOffset > destbuffer.Length)
+            newOffset = destbuffer.Length;
+
+        for (; offset < newOffset; ++offset)
+        {
+            // This is gonna clip like crazy but I don't have enough lines this commit to make it nice
+            if (on)
+                destbuffer[offset] = wavetable[offset % wavetable.Length];
+            else
+                destbuffer[offset] = 0.0;
+        }
+    }
+}
