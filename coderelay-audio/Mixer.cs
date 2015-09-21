5d9c9c4e4c571ed1622b2b0b41dbcecd5065a6db
Rohan Singh <rohan-singh@hotmail.com>
2015-09-21 15:50:45 -0400

    Add a bad mixer and square wave

---
+++ coderelay-audio/Mixer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Mixer
+{
+    public static double[] Mix(params Func<IList<double>>[] sampleGenerators)
+    {
+        return Mix(sampleGenerators.Select(f => f()).ToArray());
+    }
+
+    public static double[] Mix(params IList<double>[] sampleBuffers)
+    {
+        var outputLength = sampleBuffers.Max(a => a.Count);
+        var output = new double[outputLength];
+
+        foreach (var samples in sampleBuffers)
+        {
+            for (var i = 0; i < samples.Count; i++)
+            {
+                output[i] += samples[i];
+            }
+        }
+
+        for (var i = 0; i < outputLength; i++)
+        {
+            output[i] /= sampleBuffers.Length;
+        }
+
+        return output;
+    }
+}
--- coderelay-audio/Program.cs
+++ coderelay-audio/Program.cs
@@ -11,21 +11,38 @@ class Program
 
         // Generate a single sine wave period
         const int sineSamples = 200;
-        double [] sineTable = new double[sineSamples];
+        double[] sineTable = new double[sineSamples];
         for (int i = 0; i < sineSamples; ++i)
         {
             sineTable[i] = Math.Sin(2.0 * Math.PI * (double)i / (double)sineSamples);
         }
 
         // Fill a buffer with sine wave
-        double[] finaldata = new double[SampleRate * 2];
-        for (int i = 0; i < finaldata.Length; ++i)
+        double[] sineData = new double[SampleRate * 2];
+        for (int i = 0; i < sineData.Length; ++i)
         {
-            finaldata[i] = sineTable[i % sineSamples];
+            sineData[i] = sineTable[i % sineSamples];
         }
 
+        // Generate a single square wave period
+        const int squareSamples = 200;
+        double[] squareTable = new double[squareSamples];
+        for (int i = 0; i < squareSamples; ++i)
+        {
+            squareTable[i] = Math.Sin(2.0 * Math.PI * (double)i / (double)sineSamples) > 0 ? 1 : 0;
+        }
+
+        // Fill a buffer with square wave
+        double[] squareData = new double[SampleRate];
+        for (int i = 0; i < squareData.Length; ++i)
+        {
+            squareData[i] = squareTable[i % squareSamples];
+        }
+
+        double[] finalData = Mixer.Mix(sineData, squareData);
+
         // Generate file
-        WavFile.WriteFile(arguments.Output, SampleRate, finaldata, finaldata);
+        WavFile.WriteFile(arguments.Output, SampleRate, finalData, finalData);
 
         // Execute post gen argument
         if (string.IsNullOrEmpty(arguments.ExecAfter) == false)
