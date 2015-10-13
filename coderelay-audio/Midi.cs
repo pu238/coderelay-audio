a86d8245c592b52a6a8824d7515e06ef807ec073
Spanfile <santeri.kasvi@gmail.com>
2015-10-13 22:19:04 +0300

    Added Midi.Note()

---
+++ coderelay-audio/Midi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum Note
+{
+    C, CSharp, D, DSharp, E, F, FSharp, G, GSharp, A, ASharp, B
+}
+
+public static class Midi
+{
+    public static int Note(Note note, int octave) => (int)note + (12 * octave);
+}
